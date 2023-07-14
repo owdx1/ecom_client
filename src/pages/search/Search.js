ef12ce52647e53ac85401df6381cb737f7d1c96d
Can Bozkurt <can.bozkurt.dhf@gmail.com>
2023-07-15 02:02:56 +0300

    detaylı search yapmak için Search.js eklendi 'gotum cıktı'

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -11,6 +11,27 @@ const Search = () => {
   const [originalProducts, setOriginalProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [selectedSize, setSelectedSize] = useState('');
+  const [selectedColor, setSelectedColor] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [activeButton, setActiveButton] = useState('name');
+
+  const sizes = ['XXS', 'XS', 'S', 'M', 'L', 'XL', 'XXL'];
+  const colors = [
+    'beyaz', 'acik_mavi', 'parlament_mavisi', 'turkuaz', 'duman_grisi', 'gri', 'lacivert',
+    'petrol_mavisi', 'petrol_yesili', 'kuf_yesili', 'benetton_yesili', 'ameliyathane_yesili',
+    'pembe', 'lila', 'mor', 'fuchsia', 'kirmizi', 'siyah', 'saks_mavisi', 'fistik_yesili',
+    'bordo', 'nar_cicegi', 'fume', 'murdum', 'acik_petrol_yesili', 'avci_yesili', 'ozel_mor',
+    'su_yesili', 'visne', 'leylak', 'sari', 'hardal', 'kiremit', 'gul_kurusu', 'somon',
+    'haki', 'menekse', 'kot_mavisi', 'bej', 'kahverengi', 'kum_rengi', 'turuncu_turkuaz',
+    'mint_yesili', 'mavi', 'krem', 'antep_fistigi'
+  ];
+  const categories = [
+    'takim',
+    'tek-ust',
+    'tek-alt',
+    'tesettur'
+  ];
 
   useEffect(() => {
     const fetchOriginalProducts = async () => {
@@ -19,6 +40,7 @@ const Search = () => {
         if (response.ok) {
           const { data } = await response.json();
           setOriginalProducts(data);
+          originalProducts.map((prod) => console.log(prod))
         } else {
           throw new Error('An error occurred while fetching original products');
         }
@@ -31,16 +53,18 @@ const Search = () => {
   }, []);
 
   useEffect(() => {
-    if (searchParam && originalProducts.length > 0) {
+    if (searchParam && originalProducts.length > 0 && activeButton === 'name') {
       const filtered = originalProducts.filter((product) =>
         product.product_name.toLowerCase().includes(searchParam.toLowerCase())
       );
       setFilteredProducts(filtered);
     }
-  }, [searchParam, originalProducts]);
+  }, [searchParam, originalProducts, activeButton]);
 
-  const handleSearch = (e) => {
+  const handleSearchByName = (e) => {
     e.preventDefault();
+    setActiveButton('name');
+
     if (searchTerm.trim() !== '') {
       const filtered = originalProducts.filter((product) =>
         product.product_name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -49,6 +73,32 @@ const Search = () => {
 
       navigate(`/search?search_parameter=${encodeURIComponent(searchTerm)}`);
     }
+  };
+
+  const handleSearchByFeatures = (e) => {
+    e.preventDefault();
+    setActiveButton('features');
+
+    const filtered = originalProducts.filter((product) =>
+      (selectedSize === '' || product.size === selectedSize) &&
+      (selectedColor === '' || product.color === selectedColor) 
+     // && (selectedCategory === '' || product.category === selectedCategory) simdilik kategori bir ise yaramıyo bu dursun bi
+    );
+    setFilteredProducts(filtered);
+
+    const queryParams = new URLSearchParams();
+    if (selectedSize !== '') {
+      queryParams.set('size', encodeURIComponent(selectedSize));
+    }
+    if (selectedColor !== '') {
+      queryParams.set('color', encodeURIComponent(selectedColor));
+    }
+    if (selectedCategory !== '') {
+      queryParams.set('category', encodeURIComponent(selectedCategory));
+    }
+
+    const searchParamsString = queryParams.toString();
+    navigate(`/search?${searchParamsString}`);
   };
 
   return (
@@ -60,38 +110,82 @@ const Search = () => {
       )}
       <div className="search-container">
         <div className="search-options">
-          <form onSubmit={handleSearch}>
-            <input
-              type="text"
-              placeholder="Search..."
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
-            <button type="submit">Search</button>
-            <div className="size-options">
-              <label>
-                <input type="submit" name="size" value="XXS" />
-              </label>
-              <label>
-                <input type="submit" name="size" value="XS" />
-              </label>
-              <label>
-                <input type="submit" name="size" value="S" />
-              </label>
-              <label>
-                <input type="submit" name="size" value="M" />
-              </label>
-              <label>
-                <input type="submit" name="size" value="L" />
-              </label>
-              <label>
-                <input type="submit" name="size" value="XL" />
-              </label>
-              <label>
-                <input type="submit" name="size" value="XXL" />
-              </label>
-            </div>
-          </form>
+          <div className="search-buttons">
+            <button
+              className={`search-button ${activeButton === 'name' ? 'active' : ''}`}
+              onClick={() => setActiveButton('name')}
+            >
+              İsme göre ara
+            </button>
+            <button
+              className={`search-button ${activeButton === 'features' ? 'active' : ''}`}
+              onClick={() => setActiveButton('features')}
+            >
+              Özelliğe göre ara
+            </button>
+          </div>
+          {activeButton === 'name' ? (
+            <form onSubmit={handleSearchByName}>
+              <input
+                type="text"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+              <button type="submit">Ara</button>
+            </form>
+          ) : (
+            <form onSubmit={handleSearchByFeatures}>
+              <div className="search-features">
+                <div className="search-feature">
+                  <span>Size:</span>
+                  <div className='size-options'>
+                    {sizes.map((size) => (
+                      <label key={size}>
+                        <input
+                          type="button"
+                          onClick={() => setSelectedSize(size === selectedSize ? '' : size)}
+                          value={size}
+                          className={`filter-button ${size === selectedSize ? 'active' : ''}`}
+                        />
+                      </label>
+                    ))}
+                  </div>
+                </div>
+                <div className="search-feature">
+                  <span>Color:</span>
+                  <div className="color-options">
+                    {colors.map((color) => (
+                      <label key={color}>
+                        <input
+                          type="button"
+                          onClick={() => setSelectedColor(color === selectedColor ? '' : color)}
+                          value={color}
+                          className={`filter-button ${color === selectedColor ? 'active' : ''}`}
+                        />
+                      </label>
+                    ))}
+                  </div>
+                </div>
+                <div className="search-feature">
+                  <span>Category:</span>
+                  <div className="category-options">
+                    {categories.map((category) => (
+                      <label key={category}>
+                        <input
+                          type="button"
+                          onClick={() => setSelectedCategory(category === selectedCategory ? '' : category)}
+                          value={category}
+                          className={`filter-button ${category === selectedCategory ? 'active' : ''}`}
+                        />
+                      </label>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <button type="submit">Ara</button>
+            </form>
+          )}
         </div>
       </div>
 
@@ -103,16 +197,13 @@ const Search = () => {
                 <img src={dummyImage} alt={product.product_id} />
               </div>
               <div className="product-details-main">
-                
                 <div className="first-detail">
                   <p>{product.product_name}</p>
                 </div>
-                  
-                
                 {product.quantity <= 4 && product.quantity !== 0 ? <p>Son {product.quantity} ürün!</p> : null}
                 {product.quantity > 0 ? <p>Stokta ✔️</p> : <p>Stokta değil ❌</p>}
                 <div className="product-price-div">
-                    <p className="product-price">{product.price} TL</p>
+                  <p className="product-price">{product.price} TL</p>
                 </div>
               </div>
             </div>
